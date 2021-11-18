b4d6214062ee451a385fbdce0dbbbbc5023d25c7
tomyamGTX <hakimimdnoor90@gmail.com>
2021-11-18 13:54:09 +0800

    test riv with simple design

--- lib/learner/Screens/al_fatihah/alfatihah.comic.screen.dart
+++ lib/learner/Screens/al_fatihah/alfatihah.comic.screen.dart
@@ -1,10 +1,14 @@
 /// Demonstrates playing a one-shot animation on demand
 
+import 'package:delayed_display/delayed_display.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:learningapp/Services/Firebase.comic.dart';
 import 'package:learningapp/learner/model/Comic.models.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:rive/rive.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class PlayPauseAnimation extends StatefulWidget {
   const PlayPauseAnimation({Key? key}) : super(key: key);
@@ -15,6 +19,8 @@ class PlayPauseAnimation extends StatefulWidget {
 
 class _PlayPauseAnimationState extends State<PlayPauseAnimation> {
   String title = '';
+  int pageChanged = 0;
+  PageController pageController = PageController();
 
   // Controller for playback
   late RiveAnimationController _controller;
@@ -40,13 +46,18 @@ class _PlayPauseAnimationState extends State<PlayPauseAnimation> {
 
   @override
   Widget build(BuildContext context) {
+    var titles = [
+      "Hi, Lets learn arabic together",
+      "Ahlan Wasahalan !",
+      "Kaifahaluka ?",
+    ];
     return Scaffold(
-      backgroundColor: const Color(0xFFeb3a60),
+      backgroundColor: const Color(0xFFfaf5f5),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           title,
-          style: const TextStyle(color: black,fontWeight: FontWeight.bold),
+          style: const TextStyle(color: black, fontWeight: FontWeight.bold),
         ),
       ),
       body: StreamBuilder<Comic>(
@@ -55,27 +66,125 @@ class _PlayPauseAnimationState extends State<PlayPauseAnimation> {
           if (snapshot.hasData) {
             Comic? comic = snapshot.data;
 
-            return Center(
-              child: RiveAnimation.network(
-                comic!.url,
-                controllers: [_controller],
-                // Update the play state when the widget's initialized
-                onInit: (_) => setState(() {
-                  changeText(comic.language);
-                }),
-              ),
+            return Stack(
+              children: [
+                Container(
+                  height: 1000,
+                  padding: const EdgeInsets.only(right: 280.0),
+                  child: RiveAnimation.asset(
+                    '${comic!.url}',
+                    controllers: [_controller],
+                    // Update the play state when the widget's initialized
+                    onInit: (_) => setState(() {
+                      changeText(comic.language);
+                    }),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: PageView(
+                        controller: pageController,
+                        onPageChanged: (value) {
+                          setState(() => pageChanged = value);
+                        },
+                        children: [
+                          DelayedDisplay(
+                            delay: const Duration(milliseconds: 500),
+                            child: Center(
+                                child: Container(
+                              width: 500,
+                              padding:
+                              const EdgeInsets.only(top: 160.0, left: 180),
+                              alignment: Alignment.topCenter,
+                              child: Text(
+                                titles[pageChanged],
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: 40.0),
+                              ),
+                            )),
+                          ),
+                          DelayedDisplay(
+                            delay: const Duration(milliseconds: 500),
+                            child: Center(
+                                child: Container(
+                              width: 500,
+                              padding:
+                              const EdgeInsets.only(top: 160.0, left: 180),
+                              alignment: Alignment.topCenter,
+                              child: Text(
+                                titles[pageChanged],
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: 40.0),
+                              ),
+                            )),
+                          ),
+                          DelayedDisplay(
+                            delay: const Duration(milliseconds: 500),
+                            child: Center(
+                                child: Container(
+                              width: 500,
+                              padding:
+                                  const EdgeInsets.only(top: 160.0, left: 180),
+                              alignment: Alignment.topCenter,
+                              child: Text(
+                                titles[pageChanged],
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: 40.0),
+                              ),
+                            )),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: 300,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            if (pageChanged != 0)
+                              GestureDetector(
+                                onTap: () {
+                                  pageChanged = pageChanged - 1;
+
+                                  pageController.animateToPage(pageChanged,
+                                      duration: const Duration(milliseconds: 500),
+                                      curve: Curves.easeInOut);
+                                },
+                                child: const Icon(Icons.arrow_back,
+                                    color: Color(0xff430444), size: 50),
+                              ),
+                            const Spacer(),
+                            if (pageChanged != 3)
+                              GestureDetector(
+                                onTap: () {
+                                  pageChanged = pageChanged + 1;
+                                  if (pageChanged >= 3) {
+                                    pageChanged = pageChanged - 1;
+                                  } else {
+                                    pageController.animateToPage(pageChanged,
+                                        duration:
+                                            const Duration(milliseconds: 500),
+                                        curve: Curves.linearToEaseOut);
+                                  }
+                                },
+                                child: const Icon(Icons.arrow_forward,
+                                    color: Color(0xff430444), size: 50),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             );
           } else {
             return const Center(child: Text('Loading ...'));
           }
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _togglePlay,
-        tooltip: isPlaying ? 'Pause' : 'Play',
-        child: Icon(
-          isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
       ),
     );
   }
